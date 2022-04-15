869e125bd3b964baee9840218ee7a65643fb2323
Jakob Skov - PC <jakobskovolsen@gmail.com>
2022-04-15 18:02:22 +0200

    donald duck changes

---
+++ content.js
@@ -0,0 +1,42 @@
+var allElements = [...document.body.getElementsByTagName('*')];
+
+function findAndReplace() {
+  allElements.forEach(element =>{
+    element.childNodes.forEach(child => { 
+      if (child.nodeType === 3) {
+        replaceText(child);
+      }
+    });
+  });
+}
+
+function replaceText (node) {
+  let value = node.nodeValue;
+
+  const words = [
+    {
+      search: 'Vladimir Putin',
+      replace: 'Anders And',
+    }, {
+      search: 'Putin',
+      replace: 'Anders And',
+    }, {
+      search: 'Rusland',
+      replace: 'Andeby',
+    }, {
+      search: 'missil',
+      replace: 'kosteskaft',
+    },
+  ];
+
+  words.forEach(word => {
+    if (value.includes(word.search)) {
+      const regex = new RegExp(word.search, 'gi');
+      value = value.replace(regex, word.replace);
+    }
+  })
+
+  node.nodeValue = value;
+}
+
+window.onload = findAndReplace;
